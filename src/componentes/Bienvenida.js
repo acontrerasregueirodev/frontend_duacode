a3a08b9f26107a599273d7de34befa69bbe632b6
acontrerasregueirodev <adrian.contreras.rookie@gmail.com>
2024-11-19 13:54:19 +0100

    test de vercel

--- src/componentes/Bienvenida.js
+++ src/componentes/Bienvenida.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 const Bienvenida = () => {
   const [welcomeMessage, setWelcomeMessage] = useState(""); // Estado para el mensaje
   const [csrfToken, setCsrfToken] = useState("");  // Estado para el token CSRF
