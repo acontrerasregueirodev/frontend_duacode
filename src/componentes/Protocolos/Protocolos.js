1db7c6e9f3593a34415f3403eacbd2fb1baef88c
MartinOisRod <martin.ois.rookie@gmail.com>
2024-11-20 17:56:01 +0100

    Protocolos descripcion4

--- src/componentes/Protocolos/Protocolos.js
+++ src/componentes/Protocolos/Protocolos.js
@@ -1,43 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import ProtocoloCategoria from './ProtocoloCategoria';
-import '../../styles/Protocolos/protocolos.css'; // Asegúrate de tener los estilos aquí.
+import React, { useState, useEffect } from "react";
+import ProtocoloCategoria from "./ProtocoloCategoria";
+import "../../styles/Protocolos/protocolos.css"; // Asegúrate de tener los estilos aquí.
 
 const Protocolos = () => {
   const [categorias, setCategorias] = useState([]);
+  const [descripcion, setDescripcion] = useState(""); // Agregamos el estado para la descripción.
 
   useEffect(() => {
     const data = [
       {
-        categoria: 'Acceso Oficina',
+        categoria: "Acceso Oficina",
         protocolos: [
           {
             id: 1,
-            titulo: 'Protocolo de Seguridad en la Oficina',
-            descripcion: 'Cómo acceder y las medidas de seguridad en las oficinas.',
-            enlace: '',
+            titulo: "Protocolo de Seguridad en la Oficina",
+            descripcion:
+              "Cómo acceder y las medidas de seguridad en las oficinas.",
+            enlace: "",
           },
           {
             id: 2,
-            titulo: 'Protocolo de Ingreso para Visitantes',
-            descripcion: 'Normas de acceso para visitantes y proveedores.',
-            enlace: '',
+            titulo: "Protocolo de Ingreso para Visitantes",
+            descripcion: "Normas de acceso para visitantes y proveedores.",
+            enlace: "",
           },
         ],
       },
       {
-        categoria: 'Manuales de la Empresa',
+        categoria: "Manuales de la Empresa",
         protocolos: [
           {
             id: 3,
-            titulo: 'Manual de Conducta Laboral',
-            descripcion: 'Políticas y normas de conducta laboral en la empresa.',
-            enlace: '',
+            titulo: "Manual de Conducta Laboral",
+            descripcion:
+              "Políticas y normas de conducta laboral en la empresa.",
+            enlace: "",
           },
           {
             id: 4,
-            titulo: 'Guía de Recursos Humanos',
-            descripcion: 'Recursos y guías para los empleados.',
-            enlace: '',
+            titulo: "Guía de Recursos Humanos",
+            descripcion: "Recursos y guías para los empleados.",
+            enlace: "",
           },
         ],
       },
@@ -48,7 +51,8 @@ const Protocolos = () => {
 
   const handleFileUploadSuccess = (categoriaIndex, protocoloIndex, newFile) => {
     const updatedCategorias = [...categorias];
-    updatedCategorias[categoriaIndex].protocolos[protocoloIndex].enlace = newFile.url || ''; // Actualiza el enlace
+    updatedCategorias[categoriaIndex].protocolos[protocoloIndex].enlace =
+      newFile.url || ""; // Actualiza el enlace
     setCategorias(updatedCategorias);
   };
 
@@ -64,9 +68,25 @@ const Protocolos = () => {
         />
       ))}
       <div className="file-upload">
-        <input type="file" onChange={(e) => console.log('Archivo seleccionado:', e.target.files[0])} />
-        <button onClick={() => console.log('Subir archivo')}>Subir Archivo</button>
-        <textarea ></textarea>
+        <input
+          type="file"
+          onChange={(e) =>
+            console.log("Archivo seleccionado:", e.target.files[0])
+          }
+        />
+        <button onClick={() => console.log("Subir archivo")}>
+          Subir Archivo
+        </button>
+        <div>
+          <label htmlFor="descripcion">Descripción del Proyecto:</label>
+          <input
+            type="text"
+            id="descripcion"
+            name="descripcion"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
+          />
+        </div>
       </div>
     </div>
   );
