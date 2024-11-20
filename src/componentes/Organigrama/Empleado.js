13110ca14357575f3a5dd12bf542a450a27bbd45
MartinOisRod <martin.ois.rookie@gmail.com>
2024-11-20 14:19:39 +0100

    Organigrama3

--- src/componentes/Organigrama/Empleado.js
+++ src/componentes/Organigrama/Empleado.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Empleado = ({ empleado }) => {
+  const [roleName, setRoleName] = useState("");
+
+  useEffect(() => {
+    const fetchRoleName = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/api/roles/");
+        const roles = response.data;
+
+        const role = roles.find((role) => role.id === empleado.rol);
+        if (role) {
+          setRoleName(role.nombre);
+        } else {
+          setRoleName("Rol no encontrado");
+        }
+      } catch (error) {
+        console.error("Error al obtener los roles:", error);
+        setRoleName("Error al cargar rol");
+      }
+    };
+
+    fetchRoleName();
+  }, [empleado.rol]);
+
   return (
     <div className="empleado-card">
       <img src={empleado.foto} alt={empleado.nombre} className="empleado-foto" />
       <h3>{empleado.nombre}</h3>
-      <p>{empleado.perfil}</p>
-      <p>{empleado.email}</p>
+      <p>
+        <strong>Puesto:</strong> {roleName}
+      </p>
+      <p>
+        <strong>Email:</strong> {empleado.email}
+      </p>
     </div>
   );
 };
