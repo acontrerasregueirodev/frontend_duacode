a7103264349e814b211b8ec813b3b85b530820d2
MartinOisRod <martin.ois.rookie@gmail.com>
2024-11-21 11:26:56 +0100

    Intento protocolos4

--- src/componentes/Protocolos/Protocolos.js
+++ src/componentes/Protocolos/Protocolos.js
@@ -6,32 +6,35 @@ const Protocolos = () => {
   const [protocolos, setProtocolos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const nombre = 'protocolos';
+  const [nombre, setNombre] = useState(''); // Estado para el nombre del archivo
 
   useEffect(() => {
-    setLoading(true);
-    fetch(`https://belami.pythonanywhere.com/media/${nombre}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-          throw new Error('Expected JSON response, but got something else');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setProtocolos(data); 
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setLoading(false);
-      });
+    if (nombre) {
+      setLoading(true);
+      fetch(`https://belami.pythonanywhere.com/media/${nombre}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const contentType = response.headers.get("content-type");
+          if (!contentType || !contentType.includes("application/json")) {
+            throw new Error('Expected JSON response, but got something else');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setProtocolos(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading(false);
+        });
+    }
   }, [nombre]);
 
   const handleFileUploadSuccess = (newFile) => {
+    setNombre(newFile.name); // Asigna el nombre del archivo al estado
     setProtocolos((prevProtocolos) => [
       ...prevProtocolos,
       {
