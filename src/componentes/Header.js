85768000a035ee258c300874fd94142c61d2487a
MartinOisRod <martin.ois.rookie@gmail.com>
2024-11-21 09:47:13 +0100

    Arreglo fotos salas

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/inicio.css';
+import IconoLogin from '../assets/login.svg'; 
 
-//Código de la cabecera
-const Header = ({ menuOpen, setMenuOpen, searchVisible, setSearchVisible, searchTerm, setSearchTerm }) => {
+const Header = ({ menuOpen, setMenuOpen }) => {
+  const navigate = useNavigate(); 
+
   return (
     <header className={`header ${menuOpen ? "menu-open" : ""}`}>
       <div
@@ -17,6 +20,12 @@ const Header = ({ menuOpen, setMenuOpen, searchVisible, setSearchVisible, search
         <h1 className="brand-title">
           duacode<span className="highlight">.</span>
         </h1>
+        <img
+          src={IconoLogin}
+          alt="Inicio de sesión"
+          className="icono-login"
+          onClick={() => navigate('https://duacode.vercel.app/login')} 
+        />
       </div>
     </header>
   );
